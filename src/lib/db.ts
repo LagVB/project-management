e0b6310521d31e72ed04adb7771c55c0ff284539
Luiz Augusto Vieira Bosco <lagvbosco@outlook.com>
2025-05-31 10:31:46 -0300

    Changed to prisma client file

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+    return new PrismaClient()
 }
 
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export const db = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
